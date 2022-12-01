d65659b19cf6049aa34bfb0b6058f260eb911395
Robert White <white@csdisco.com>
2022-12-01 10:23:54 -0500

    Day 1, 2022 Advent Challenge

---
+++ day1.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const _ = require('lodash');
+
+const file = fs.readFileSync('day1.input', 'utf-8');
+let elves = [];
+let currentElf = 0;
+
+file.split(/r?\n/).forEach((line) => {
+    if (line.trim() != '') {
+        if (!elves[currentElf]) {
+            elves[currentElf] = { id: currentElf, totalCals: 0, cals: [] };
+        }
+        elves[currentElf].cals.push(parseInt(line));
+        elves[currentElf].totalCals += parseInt(line);
+    } else {
+        currentElf++;
+    }
+});
+
+elves = _.sortBy(elves, (elf) => elf.totalCals).reverse();
+
+console.log(`Number of Elves: ${currentElf}`);
+console.log(
+    `Most cals held by one Elf (id: ${elves[0].id}): ${elves[0].totalCals}`
+);
+console.log(
+    `Cals held by top three Elves: ${_(elves)
+        .take(3)
+        .map((x) => x.totalCals)
+        .sum()}`
+);
+console.log(
+    `Total Cals: ${_(elves)
+        .map((x) => x.totalCals)
+        .sum()}`
+);
